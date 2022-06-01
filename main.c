20170a81481094e31a5f2d757219760425905d67
Roberto Sousa <robert6257@gmail.com>
2022-06-01 16:56:09 -0300

    Criando variaveis e estrutura inicial

--- main.c
+++ main.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
+
+#define tamanho 5
+
+//Estrutura da Fila
+struct tfila{
+    int dados[tamanho];
+    int ini;
+    int fim;
+};
+
+//Variaveis
+struct tfila fila;
+int op;
+
+//prototipos
+void fila_entrar();
+void fila_sair();
+void fila_mostrar();
+void menu_mostrar();
 
 int main()
 {
     printf("Hello world!\n");
     return 0;
+}
+
+void fila_entrar(){
+
+}
+
+void fila_sair(){
+
+}
+
+void fila_mostrar(){
+
+}
+
+void menu_mostrar(){
+
 }
